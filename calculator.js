d0f538cd0f43c6282c0ee1f8242d26aa993de1c1
akash <akashrp512@gmail.com>
2022-11-17 14:50:19 +0530

    Bmi calculator added

--- calculator.js
+++ calculator.js
@@ -10,15 +10,31 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", (req,res)=>{
+app.get("/bmicalculator", (req, res) => {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
 
-    var num1 = Number(req.body.n1);
-    var num2 = Number(req.body.n2);
+app.post("/", (req, res)=>{
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
 
     var result = num1 + num2 ;
 
     res.send("The results of the calculation is " + result );
 });
+
+app.post("/bmicalculator", (req ,res)=>{
+
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+
+  var bmi = weight / (height * height);
+
+  res.send("Your BMI is " + bmi );
+});
+
+
 
 app.listen(port , ()=>{
     console.log('appp listening on port ${port}')
